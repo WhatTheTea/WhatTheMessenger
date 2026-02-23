93b8de268b3d2eacaec31ce4e94f5d881af1b27a
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2026-02-23 21:03:59 +0200

    Implement messaging proto

---
+++ WhatTheMessenger.Application/Models/NewMessageModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WhatTheMessenger.Application.Models;
+
+public sealed record NewMessageModel
+{
+    [StringLength(int.MaxValue, MinimumLength = 1)]
+    public required string Content { get; set; } = string.Empty;
+}
