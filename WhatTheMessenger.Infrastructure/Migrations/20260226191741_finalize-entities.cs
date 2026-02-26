af33d7e3a778d9ab460f4f1e4cffe8d01f0dfe83
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2026-02-26 21:41:58 +0200

    Update schema

---
+++ WhatTheMessenger.Infrastructure/Migrations/20260226191741_finalize-entities.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WhatTheMessenger.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class finalizeentities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ChatUser_AspNetUsers_ParticipantsId",
+                table: "ChatUser");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_AspNetUsers_RecipientId",
+                table: "Messages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Chats_ChatId",
+                table: "Messages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Messages_RecipientId",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "RecipientId",
+                table: "Messages");
+
+            migrationBuilder.RenameColumn(
+                name: "ParticipantsId",
+                table: "ChatUser",
+                newName: "UsersId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ChatUser_ParticipantsId",
+                table: "ChatUser",
+                newName: "IX_ChatUser_UsersId");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ChatId",
+                table: "Messages",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ChatUser_AspNetUsers_UsersId",
+                table: "ChatUser",
+                column: "UsersId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Chats_ChatId",
+                table: "Messages",
+                column: "ChatId",
+                principalTable: "Chats",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ChatUser_AspNetUsers_UsersId",
+                table: "ChatUser");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Chats_ChatId",
+                table: "Messages");
+
+            migrationBuilder.RenameColumn(
+                name: "UsersId",
+                table: "ChatUser",
+                newName: "ParticipantsId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ChatUser_UsersId",
+                table: "ChatUser",
+                newName: "IX_ChatUser_ParticipantsId");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ChatId",
+                table: "Messages",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "RecipientId",
+                table: "Messages",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_RecipientId",
+                table: "Messages",
+                column: "RecipientId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ChatUser_AspNetUsers_ParticipantsId",
+                table: "ChatUser",
+                column: "ParticipantsId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_AspNetUsers_RecipientId",
+                table: "Messages",
+                column: "RecipientId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Chats_ChatId",
+                table: "Messages",
+                column: "ChatId",
+                principalTable: "Chats",
+                principalColumn: "Id");
+        }
+    }
+}
