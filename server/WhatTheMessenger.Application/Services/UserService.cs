2a06fde3acb73b9c19e85f071a3e85469eecd940
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2026-09-18 22:30:50 +0300

    Implement user API

--- server/WhatTheMessenger.Application/Services/UserService.cs
+++ server/WhatTheMessenger.Application/Services/UserService.cs
@@ -7,13 +7,19 @@ namespace WhatTheMessenger.Application.Services;
 public interface IUserService
 {
     Task<User[]> FindUsersAsync(string query, CancellationToken token = default);
+
+    Task<User?> GetUserAsync(Guid id);
 }
 
 public class UserService(IAppDbContext dbContext) : IUserService
 {
     public Task<User[]> FindUsersAsync(string query, CancellationToken token = default) => dbContext.Users
+        .AsNoTracking()
         .Where(u => u.UserName!.Contains(query) ||
                     u.DisplayName!.Contains(query))
         .Take(20)
         .ToArrayAsync(cancellationToken: token);
+
+    public Task<User?> GetUserAsync(Guid id) => dbContext.Users.AsNoTracking()
+        .SingleOrDefaultAsync(x => x.Id == id);
 }
---
+++ server/WhatTheMessenger.Server/Api/User.cs
@@ -0,0 +1,35 @@
+using WhatTheMessenger.Application.Models;
+using WhatTheMessenger.Application.Services;
+
+namespace WhatTheMessenger.Server.Api;
+
+public static class Users
+{
+    extension (WebApplication app)
+    {
+        public WebApplication MapUserEndpoints()
+        {
+            var group = app.MapGroup("/api/users");
+
+            group.MapGet("/{id}", async (Guid id, IUserService userService) => 
+                {
+                    var result = userService.GetUserAsync(id);
+                    
+                    return result is not null ? Results.Ok(result)
+                        : Results.NotFound();
+                })
+                .RequireAuthorization()
+                .Produces(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status404NotFound)
+                .Produces(StatusCodes.Status401Unauthorized);
+
+            group.MapGet("/search/{query}", async (string query, IUserService userService) => userService.FindUsersAsync(query))
+                .RequireAuthorization()
+                .Produces(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status404NotFound)
+                .Produces(StatusCodes.Status401Unauthorized);
+
+            return app;
+        }
+    }
+}
